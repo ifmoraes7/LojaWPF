dc46e588b4382f63e8d5106d7d23b089410cb70e
Iagao-PC <iagofmoraes@hotmail.com>
2022-09-21 17:31:19 -0300

    Alteração novamente nas telas de cadastramento de clientes e de listagem de clientes

---
+++ LojaPrjWPF/Telas/ClienteCad.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace LojaPrjWPF.Telas
+{
+    partial class ClienteCad
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(718, 487);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ClienteCad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Name = "ClienteCad";
+            this.Size = new System.Drawing.Size(793, 549);
+            this.Load += new System.EventHandler(this.ClienteCad_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
--- LojaPrjWPF/Telas/MainWindow.xaml.cs
+++ LojaPrjWPF/Telas/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LojaPrjWPF.Telas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,10 +29,55 @@ namespace LojaPrjWPF
 
         private void btnCadastroCliente_Click(object sender, RoutedEventArgs e)
         {
-            
+           // CadCliente cadcliente = new CadCliente();
+           // object clientes1 = cadcliente.Content;
+            //ClienteCad clientecad = new ClienteCad();
+            //grdCadCliente.Children.Add(cadcliente.Content);
+
+
         }
 
         private void btnListarClientes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCadastrarPedido_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click_2(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnListarPedidos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListarProdutos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProdutos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCadastrarProduto_Click(object sender, RoutedEventArgs e)
         {
 
         }
